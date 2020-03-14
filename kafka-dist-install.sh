fb0e5a2603831d055b551b68b7248502b2856dd9
Uwe Eisele <code@uweeisele.eu>
2020-03-15 00:10:25 +0100

    WIP: create active bin links

--- kafka-dist-install.sh
+++ kafka-dist-install.sh
@@ -12,31 +12,34 @@ SCALA_VERSIONS=("2.13" "2.12" "2.11")
 ARCHIVE_DIR=${SCRIPT_DIR}/archive
 DIST_DIR=${SCRIPT_DIR}/dist
 BIN_DIR=${SCRIPT_DIR}/bin
+BIN_ALL_DIR=${BIN_DIR}/all
+BIN_ACTIVE_DIR=${BIN_DIR}/active
 
 function usage () {
     echo "$0: $1" >&2
     echo
-    echo "Usage: $0 <distribution (apache|cp)> <version>"
+    echo "Usage: $0 <distribution (apache|cp)> <version> [active|inactive]"
     echo
 }
 
 function install_kafka_distribution() {
     local dist=${1:?"Missing distribution type as first parameter! Allowed values are 'apache' and 'cp'."}
     local version=${2:?"Missing the name of the distribution as first parameter!"}
+    local state=${3:-"active"}
 
     local actual_dist_dir="${DIST_DIR}/${dist}-kafka-${version}"
 
-    if [[ -d "${actual_dist_dir}" ]]; then
-        echo "Version ${version} of Distribution ${dist} is already installed!"
-        #exit 1
+    if [[ ! -d "${actual_dist_dir}" ]]; then
+        local archive=$(download_dist ${dist} ${version})
+        extract_dist ${archive} ${actual_dist_dir}
+        #delete_archive ${archive}
+        patch_scripts ${dist} ${version} ${actual_dist_dir}
+        link_to_bin_dir ${dist} ${version} ${actual_dist_dir}
     fi
 
-    local archive=$(download_dist ${dist} ${version})
-
-    extract_dist ${archive} ${actual_dist_dir}
-    patch_scripts ${dist} ${version} ${actual_dist_dir}
-
-    link_to_bin_dir ${dist} ${version} ${actual_dist_dir}
+    if [ "${state}" == "active" ]; then
+        link_to_active_bin_dir ${actual_dist_dir}
+    fi
 }
 
 function download_dist() {
@@ -69,6 +72,11 @@ function extract_dist() {
     tar -xf ${archive} -C ${actual_dist_dir} --strip-components=1
 }
 
+function delete_archive() {
+    local archive=$1
+    rm ${archive}
+}
+
 function patch_scripts() {
     local actual_dist_dir=$3
 
@@ -82,9 +90,21 @@ function link_to_bin_dir() {
     local version=$2
     local actual_dist_dir=$3
 
-    mkdir -p ${BIN_DIR}
+    mkdir -p ${BIN_ALL_DIR}
     for cmd in $(find ${actual_dist_dir}/bin -maxdepth 1 -type f); do
-        ln -s ${cmd} ${BIN_DIR}/${dist}-$(basename ${cmd})-${version}
+        local cmd_basename=$(basename ${cmd})
+        ln -sf ${cmd} ${BIN_ALL_DIR}/${dist}-${cmd_basename%.*}-${version}
+    done
+}
+
+function link_to_active_bin_dir() {
+    local actual_dist_dir=$1
+
+    rm -f ${BIN_ACTIVE_DIR}/*
+    mkdir -p ${BIN_ACTIVE_DIR}
+    for cmd in $(find ${actual_dist_dir}/bin -maxdepth 1 -type f); do
+        local cmd_basename=$(basename ${cmd})
+        ln -s ${cmd} ${BIN_ACTIVE_DIR}/${cmd_basename%.*}
     done
 }
 
